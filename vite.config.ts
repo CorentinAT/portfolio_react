211765ba20fd1585659e0d13e1d6149b976f8988
CorentinAT <corentin.al2004@gmail.com>
2025-01-18 16:28:44 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@pages': '/src/pages',
+			'@models': '/src/models',
+			'@components': '/src/components',
+			'@content': '/src/content'
+		}
+	}
+})
