eeb473cb36a9f43f3d0c290cbe7858e60a93b224
CorentinAT <corentin.al2004@gmail.com>
2025-04-07 00:54:45 +0200

    rendu correct

--- src/pages/DevPage.tsx
+++ src/pages/DevPage.tsx
@@ -2,22 +2,25 @@ import NavBar from '@components/NavBar'
 
 import '@styles/devPage.css'
 
-import Project from '@components/dev/project/Project'
-import InfoBubble from '@components/infoBubble/InfoBubble'
+// import Project from '@components/dev/project/Project'
+// import InfoBubble from '@components/infoBubble/InfoBubble'
 
-import { infos } from '@content/infos'
-import { projects } from '@content/projects'
+// import { infos } from '@content/infos'
+// import { projects } from '@content/projects'
 
 export default function DevPage() {
 	return (
 		<>
 			<NavBar />
-			<InfoBubble text={infos.dev} left='400px' top='250px' />
+			<div className='dev-container'>
+				<p>Work in Progress...</p>
+				{/* <InfoBubble text={infos.dev} left='400px' top='250px' />
 			<div className='dev-container'>
 				<h1>Informatique</h1>
 				{projects.map((project) => (
 					<Project project={project} />
 				))}
+			</div> */}
 			</div>
 		</>
 	)
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,58 +1,51 @@
 import '@styles/homePage.css'
 
-import { useState } from 'react'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import { Link } from 'react-router-dom'
-
-import dev from '@assets/dev.webp'
-import tpose from '@assets/tpose.gif'
-import videos from '@assets/videos.webp'
 
 import InfoBubble from '@components/infoBubble/InfoBubble'
 
 import { infos } from '@content/infos'
 
 export default function HomePage() {
-	const [devImageVisibility, setDevImageVisibility] = useState('')
-	const [videosImageVisibility, setVideosImageVisibility] = useState('')
-
 	return (
 		<div className='home-container'>
-			<div>
-				<h1>Corentin ALBERT</h1>
-				<div className='tpose'>
-					<img src={tpose} />{' '}
-				</div>
-				<InfoBubble text={infos.home} left='40%' top='60%' />
+			<InfoBubble text={infos.home} left='40%' top='15%' />
+
+			<div className='home-content'>
+				<a href='cv.pdf' download='cv_corentin_albert.pdf'>
+					<h1>
+						Corentin
+						<br />
+						Albert
+					</h1>
+				</a>
 			</div>
-			<Link
-				className='link-dev'
-				to='dev'
-				onMouseEnter={() => setDevImageVisibility('visible')}
-				onMouseLeave={() => setDevImageVisibility('invisible')}
-			>
-				Informatique
-			</Link>
-			<Link
-				className='link-videos'
-				to='videos'
-				onMouseEnter={() => setVideosImageVisibility('visible')}
-				onMouseLeave={() => setVideosImageVisibility('invisible')}
-			>
-				Vidéos
-			</Link>
-			<img className={`videos-image ${videosImageVisibility}`} src={videos} />
-			<img className={`dev-image ${devImageVisibility}`} src={dev} />
+
+			<div className='right-links'>
+				<Link to='videos' className='big-link'>
+					Vidéo
+				</Link>
+				<Link to='dev' className='big-link'>
+					Informatique
+				</Link>
+			</div>
+
 			<div className='external-links'>
-				<a href='https://github.com/corentinat' target='_blank'>
-					<GitHubIcon titleAccess='github' sx={{ fontSize: '35px' }} />
+				<a
+					href='https://github.com/corentinat'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<GitHubIcon titleAccess='GitHub' sx={{ fontSize: '35px' }} />
 				</a>
 				<a
 					href='https://www.linkedin.com/in/corentin-albert-75a939251'
 					target='_blank'
+					rel='noopener noreferrer'
 				>
-					<LinkedInIcon titleAccess='linkedin' sx={{ fontSize: '40px' }} />
+					<LinkedInIcon titleAccess='LinkedIn' sx={{ fontSize: '40px' }} />
 				</a>
 			</div>
 		</div>
